40b3f149857a1b6c189315f0dec0e7f7d95d96cd
patrykcyran <83006094+patrykcyran@users.noreply.github.com>
2023-10-13 09:56:50 +0200

    init

---
+++ src/Witaj.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Witaj extends Component {
+    render() {
+        let miejsce = "Świecie";
+        return (
+            <h1>Witaj w {miejsce}</h1>
+        )
+    }
+}
+
+export default Witaj;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ListaZakupow from './ListaZakupow';
+
+let listaKart = [
+    {   id: 1,
+        tytul: "Przeczytać książkę",
+        opis: "Muszę przeczytać całą książkę",
+        status: "in-progress",
+        zadania: [] },
+    {   id: 2,
+        tytul: "Napisać trochę kodu",
+        opis: "Będę przepisywać kod przykładu z laboratorium",
+        status: "todo",
+        zadania: [
+            {   id: 1,
+                nazwa: "Przykład listy zakupów",
+                zrobione: true },
+            {   id: 2,
+                nazwa: "Przykład Kanban",
+                zrobione: false },
+            {   id: 3,
+                nazwa: "Mój własny przykład",
+                zrobione: false }
+        ]
+    }
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    Witaj Świecie
+    <ListaZakupow />
   </React.StrictMode>
 );
